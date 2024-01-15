45f89c54f12049e3ccc4c736d247e7c981ef17d1
TIGER <141488144+tiger7815@users.noreply.github.com>
2024-01-15 10:26:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", "6334241757:AAHo1iSa_LcjP6jbbEO-Wz1H8wp9
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
-OWNER = int(os.environ.get("OWNER", 585731991))
+OWNER = int(os.environ.get("OWNER", 6567662829))
 
 LOG = -1002021098463
 
